70ef9c049be772ba5345fad0ea1a1bbfa4995cf4
Gabriel Espinheira <gabriel.espinheira@mollie.com>
2024-11-14 14:15:45 +0000

    native entrypoint

---
+++ native.ts
@@ -0,0 +1,5 @@
+export * from './lib/hooks/useAbstractChat';
+export { } from './lib/providers/ConfigDataProvider';
+export { WidgetRoot } from './lib/Root';
+export { ComponentProps } from './lib/types/components';
+export { WidgetOptions } from './lib/types/options';
